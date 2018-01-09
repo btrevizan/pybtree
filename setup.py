a5f64159767e019da0e59a6d32c8214c702f5aec
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-01-09 20:44:18 -0200

    Package file s

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='pybtree',
+    packages=['pybtree'],
+    version='1.0',
+    description='An on-disk BTree implementation in Python 3.',
+    author='Bernardo Trevizan',
+    author_email='trevizanbernardo@gmail.com',
+    url='https://github.com/btrevizan/pybtree',
+    keywords='file,binary,search,btree,on-disk,python'
+)
